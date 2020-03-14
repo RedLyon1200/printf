e130769225ade4453c1226c30a8cc24d511560e2
CRASHED_MACHINE <sagudelo1200@gmail.com>
2020-03-14 17:55:07 -0500

    final test for brach

--- _printf.c
+++ _printf.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 #include <stdarg.h>
 #include <stdlib.h>
-
+/**
+ * _printf - produces stdout according to a format
+ * @format: string
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 
