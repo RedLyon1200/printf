a4ad8cd5072e27d7c053812b9f481b192c4d7e82
Lilsherlock <potatogames15@gmail.com>
2020-03-18 03:05:54 +0000

    fix

--- _printf.c
+++ _printf.c
@@ -1,29 +1,36 @@
-#include "holberton.h"
-#include <stdarg.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
 /**
- * _printf - produces stdout according to a format
- * @format: string
- * Return: number of characters printed
+ * _printf - produces output according to a format
+ * @format: character string
+ * Return: number of characters printed excluding null byte
  */
 int _printf(const char *format, ...)
 {
+    /* declaro variables breve nea */
 	va_list arg;
-        unsigned int i = 0, j = 0;
-
+	unsigned int i, j, flag;
+	unsigned int len = 0;
+    /* Llamo a la esctructura la cual se le asigno el nombre de print_t*/
+    /* se crea un array, llamado print con el tipo de dato de la estucturia print_t */
 	print_t print[] = {
-		{"c", print_c}, {"s", print_s},
+		{"c", print_char}, {"s", print_string}, {"d", print_decimal}, {"i", print_int},
+		{NULL, NULL}
 	};
-
+    /* Aca inicia el variable function */
 	va_start(arg, format);
-
-	if (format == NULL)
+    /* Si format es igual nulo o formato en la posicion 0 es igual a % y formato en la posicion 1 es igual a dato nulo, retorna 0 */
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (0);
 
+	i = 0;
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%' && format[i + 1] != '%')
 		{
+			j = 0;
+			flag = 0;
 			while (print[j].p != NULL)
 			{
 				if (format[i + 1] == print[j].print[0])
@@ -34,7 +41,25 @@ int _printf(const char *format, ...)
 				}
 				j++;
 			}
+			if (flag == 0)
+			{
+				_putchar(format[i]);
+				len = len + 1;
+			}
 		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar('%');
+			i++;
+			len = len + 1;
+		}
+		else
+		{
+			_putchar(format[i]);
+			len = len + 1;
+		}
+		i++;
 	}
-	return (0);
+	va_end(arg);
+	return (len);
 }
