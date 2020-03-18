0207961b71aa17c0bd1bbed575f8deaf0127feb8
Lilsherlock <potatogames15@gmail.com>
2020-03-18 03:07:51 +0000

    FixUpdates

--- holberton.h
+++ holberton.h
@@ -1,6 +1,6 @@
-#ifndef F
-#define F
-
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdarg.h>
 /**
  * struct print - struct for conversion specifiers
  * @print: struct print
@@ -11,12 +11,10 @@ typedef struct print
 	char *print;
 	int (*p)();
 } print_t;
-
-
-
 int _printf(const char *format, ...);
 int _putchar(char c);
-
-int print_c(va_list arg);
-
-#endif /* Printf */
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_decimal(va_list arg);
+int print_int(va_list arg);
+#endif /* HOLBERTON_H */
