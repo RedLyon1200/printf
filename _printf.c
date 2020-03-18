25da66350d133b126ffdb418c169a141d79f654a
CRASHED_MACHINE <sagudelo1200@gmail.com>
2020-03-17 23:33:12 -0500

    Betty was need fix

--- _printf.c
+++ _printf.c
@@ -8,19 +8,29 @@
  */
 int _printf(const char *format, ...)
 {
+    /* declaro variables breve nea */
 	va_list arg;
-	unsigned int i = 0, j = 0, flag = 0, len = 0;
+	unsigned int i, j, flag;
+	unsigned int len = 0;
+    /* Llamo a la esctructura la cual se le asigno el nombre de print_t*/
+    /* se crea un array, llamado print con el tipo de dato de la estuctura print_t */
 	print_t print[] = {
-		{"c", print_char}, {"s", print_string}, {"d", print_decimal},
-		{"i", print_int}, {NULL, NULL}
+		{"c", print_char}, {"s", print_string}, {"d", print_decimal}, {"i", print_int},
+		{NULL, NULL}
 	};
+    /* Aca inicia el variable function */
 	va_start(arg, format);
+    /* Si format es igual nulo o formato en la posicion 0 es igual a % y formato en la posicion 1 es igual a dato nulo, retorna 0 */
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (0);
+
+	i = 0;
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%' && format[i + 1] != '%')
 		{
+			j = 0;
+			flag = 0;
 			while (print[j].p != NULL)
 			{
 				if (format[i + 1] == print[j].print[0])
